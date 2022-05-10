e66331114a9862e96ac69ee0768866a5f8ccd09b
IntuzNeelG <neelg@intuz.net>
2022-05-10 19:32:16 +0530

    first commit

---
+++ .slicemachine/assets/slices/BlogTitle/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/BlogTitle';
+
+export default {
+  title: 'slices/BlogTitle'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"blog_title","items":[],"primary":{"Para":[{"type":"paragraph","text":"Pariatur magna ut quis et qui Lorem consequat ipsum exercitation cupidatat. Aliquip incididunt dolore exercitation laborum exercitation non nisi cillum eiusmod nisi laboris culpa ad. Mollit commodo commodo ea laborum reprehenderit aliquip mollit consequat exercitation laboris commodo non cillum incididunt.","spans":[]}],"linkid":{"link_type":"Web","url":"https://prismic.io"}},"id":"_Default"}} />
+_Default.storyName = 'Default'
---
+++ slices/BlogTitle/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { PrismicRichText } from "@prismicio/react";
+
+const BlogTitle = ({ slice }) => <section></section>;
+
+export default BlogTitle;
